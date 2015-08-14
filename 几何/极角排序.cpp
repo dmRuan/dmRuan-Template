c7220160563e2024d6e06a1c429b767cd964d939
dmRuan <mindi.ruan@gmail.com>
2015-08-14 10:09:14 -0400

    Update some computation of geometry template

---
+++ 几何/极角排序.cpp
@@ -0,0 +1,122 @@
+/*极角排序
+ *1.找出最下左点rp（点输入过程中找）
+ *2.sort重写的cmp（注意不排第rp，以rp为基础点ba）
+ */
+
+//找最下左点
+if( sgn(P[rp].y - P[i].y) > 0
+  || (sgn(P[rp].y - P[i].y == 0)) && sgn(P[rp].x- P[i].x > 0) )
+rp = i;
+
+//重写的cmp，ba是排序的比较点
+bool cmp(Point a, Point b)
+{
+    double pa = sgn(cross(a, b, P[ba]));
+    if( pa == 0 )
+        return dis(a,P[ba]) < dis(b,P[ba]);//调节同一极角的点
+    else return pa > 0;//逆时针排序
+}
+
+
+
+/*poj 1696-Space Ant
+ *题意：求给定点的螺旋（逆时针）序列
+ */
+/*
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+const int MAXN = 50 + 5;
+const double eps = 1e-8;
+const int INF = 1<<22;
+
+int n;
+int rp, ba;
+bool vis[MAXN];
+struct Point {
+    int idx;
+    double x, y;
+    Point() {}
+    void input() {
+        scanf("%d %lf %lf", &idx, &x, &y);
+    }
+    Point (double _x, double _y) {
+        x = _x; y = _y;
+    }
+    Point(int _idx, double _x, double _y) {
+        idx = _idx; x = _x; y = _y;
+    }
+    Point operator -(const Point &b) const {
+        return Point(x-b.x, y-b.y);
+    }
+    double operator *(const Point &b) const {
+        return x*b.x + y*b.y;
+    }
+    double operator ^(const Point &b) const {
+        return x*b.y - y*b.x;
+    }
+}P[MAXN];
+
+int sgn(double x)
+{
+    return (x>eps) - (x<-eps);
+}
+double dis(Point a, Point b)
+{
+    return sqrt((a-b) * (a-b));
+}
+double cross(Point a, Point b, Point o)
+{
+    return (a-o) ^ (b-o);
+}
+bool cmp(Point a, Point b)
+{
+    double pa = sgn(cross(a, b, P[ba]));
+    if( pa == 0 )
+        return dis(a,P[ba]) < dis(b,P[ba]);
+    else return pa > 0;
+}
+
+int main()
+{
+#ifdef dmRuan
+    freopen("in.txt", "r", stdin);
+#endif // dmRuan
+    int T;
+    scanf("%d", &T);
+    while( T-- ) {
+        //initialization
+        rp = 0;
+        memset(vis, 0, sizeof(vis));
+        scanf("%d", &n);
+        for(int i = 0; i < n; i++) {
+            P[i].input();
+            if( sgn(P[rp].y - P[i].y) > 0
+               || (sgn(P[rp].y - P[i].y == 0)) && sgn(P[rp].x- P[i].x > 0) )
+                rp = i;
+        }
+
+        //solve
+        ba = 0;//基准点
+        swap(P[0], P[rp]);
+        for(int i = 1; i < n; i++) {//求逆螺旋序列
+            sort(P+i, P+n, cmp);
+            ba++;
+        }
+
+        //output
+        printf("%d", n);
+        for(int i = 0; i < n; i++)  printf(" %d", P[i].idx);
+        printf("\n");
+    }
+
+    return 0;
+}
+
+*/
---
+++ 几何/预定义.cpp
@@ -0,0 +1,88 @@
+/*点的结构体*/
+struct Point {
+    double x, y;
+    Point() {}
+    void input() {
+        scanf("%lf %lf", &x, &y);
+    }
+    Point(double _x, double _y) {
+        x = _x; y = _y;
+    }
+    Point operator +(const Point &b) const {
+        return Point(x+b.x, y+b.y);
+    }
+    Point operator -(const Point &b) const {
+        return Point(x-b.x, y-b.y);
+    }
+    //cross
+    double operator ^(const Point &b) const {
+        return x*b.y - y*b.x;
+    }
+    //dot
+    double operator *(const Point &b) const {
+        return x*b.x + y*b.y;
+    }
+    //after rotating around the origin B(radian), x and y value
+    void transXY(double B) {
+        double tx = x, ty = y;
+        x = tx*cos(B) - ty*sin(B);
+        y = tx*sin(B) - ty*cos(B);
+    }
+};
+/*线的结构体*/
+struct Line {
+    Point u, v;
+    double k;
+    Line() {}
+    void input() {
+        u.input(); v.input();
+    }
+    Line(Point _u, Point _v) {
+        u = _u; v = _v;
+    }
+    double getk() {
+        return atan2(v.y-u.y, v.x-v.x);
+    }
+    //is line intersection?
+    //0 is coincide, 1 is parallel, 2 is intersection
+    pair<int,Point> operator &(const Line &b) const
+    {
+        Point res = u;
+        if( sgn((u-v)^(b.u-b.v)) == 0 )
+        {
+            if( sgn((u-b.v)^(b.u-b.v)) == 0 )
+                return make_pair(0,res);//重合
+            else return make_pair(1,res);//平行
+        }
+        double t = ((u-b.u)^(b.u-b.v))/((u-v)^(b.u-b.v));
+        res.x += (v.x-u.x)*t;
+        res.y += (v.y-u.y)*t;
+        return make_pair(2,res);
+    }
+};
+
+/*sign*/
+int sgn(double x)
+{
+    return (x>eps) - (x<-eps);
+}
+double cross(Point a, Point b, Point o)
+{
+    return (a-o) ^ (b-o);
+}
+double dis(Point a, Point b)
+{
+    return sqrt((a-b) * (a-b));
+}
+/*is segment intersection?*/
+bool isInter(Point a, Point b, Point c, Point d)
+{
+    return
+        max(a.x,b.x) >= min(c.x,d.x) &&
+        max(a.y,b.y) >= min(c.y,d.y) &&
+        max(c.x,d.x) >= min(a.x,b.x) &&
+        max(c.y,d.y) >= min(a.y,b.y) &&
+        sgn(cross(c,b,a) * cross(d,b,a)) <= 0 &&
+        sgn(cross(a,d,c) * cross(b,d,c)) <= 0;
+}
+

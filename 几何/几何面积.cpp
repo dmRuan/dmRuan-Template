ffa6b884a83ca8914b22914b9e816c2aac48b38e
dmRuan <mindi.ruan@gmail.com>
2015-08-29 20:37:46 +0800

    Update 几何-几何面积
    Change the way to get area of triangle.

--- 几何/几何面积.cpp
+++ 几何/几何面积.cpp
@@ -1,7 +1,7 @@
-/*Èı½ÇĞÎÃæ»ı*/
-//ÒÑÖª¶¥µã×ø±ê
-double getArea(Point a, Point b, Point c)
+/*ä¸‰è§’å½¢é¢ç§¯*/
+//å·²çŸ¥é¡¶ç‚¹åæ ‡
+double getArea(Point a, Point b, Point o)
 {
-    return 0.5*fabs(b.x*c.y + a.x*b.y + a.y*c.x
-                    - b.x*a.y - c.x*b.y - a.x*c.y);
+    //return 0.5*fabs(b.x*c.y + a.x*b.y + a.y*c.x - b.x*a.y - c.x*b.y - a.x*c.y);
+    return 0.5 * fabs((a-o)^(b-o));
 }

b2d817b65a5bbb5b0e3e4b160ea055c61ab9c907
dmRuan <mindi.ruan@gmail.com>
2015-08-31 17:16:05 +0800

    Update 几何-几何面积
    Add the way to get the area of polygon

--- 几何/几何面积.cpp
+++ 几何/几何面积.cpp
@@ -1,7 +1,14 @@
 /*三角形面积*/
 //已知顶点坐标
-double getArea(Point a, Point b, Point o)
-{
-    //return 0.5*fabs(b.x*c.y + a.x*b.y + a.y*c.x - b.x*a.y - c.x*b.y - a.x*c.y);
-    return 0.5 * fabs((a-o)^(b-o));
+double getTriAre(Point a, Point b, Point o) {
+    return fabs((a-o)^(b-o)) / 2.0;
+}
+
+double getPolAre(vector<Point> pol) {
+    double ret = 0.0;
+    int nch = pol.size();
+    for(int i = 0; i < nch; i++) {
+        ret += cross(pol[i], pol[(i+1)%nch], Point(0,0));
+    }
+    return ret / 2.0;
 }
